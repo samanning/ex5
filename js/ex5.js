e617b62a4b4ef4844ccdcd8f68dbe8b1943f94b6
samanning <samanning@ucdavis.edu>
2016-02-11 00:15:40 -0800

    update all files cuz its done

--- js/ex5.js
+++ js/ex5.js
@@ -23,11 +23,20 @@ $(document).ready(function () {
     });
 
     //reset objects back to original position
-$('a#reset').click(function(){
-    $('.object').animate({
-        top: 0,
-        left: 0,
+    $('#reset a').click(function () {
+        $('.object').animate({
+            top: 0,
+            left: 0,
+        });
     });
-});              
+
+    //code to change color of reset when hover
+    $('#reset').hover(function () {
+        //change color
+        $('#reset .button').css('color', '#F37C79').css('border-color', '#F37C79');
+    }, function () {
+        //second function
+        $('#reset .button').css('color', '').css('border-color', '');
+    });
 
 }); //end of ready function
